1a2daaaa27722621a83eca603c96196c93387875
versecafe <147033096+versecafe@users.noreply.github.com>
2025-01-21 15:55:38 +0000

    class

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,35 @@ import { Elysia, t } from "elysia";
 
 class Note {
   constructor(public data: string[] = ["Moonhalo"]) {}
+
+  add(note: string) {
+    this.data.push(note);
+
+    return this.data;
+  }
+
+  remove(index: number) {
+    return this.data.splice(index, 1);
+  }
+
+  update(index: number, note: string) {
+    this.data[index] = note;
+    return this.data[index];
+  }
 }
 
-const app = new Elysia()
+export const note = new Elysia()
   .decorate("note", new Note())
   .get("/note", ({ note }) => note.data)
+  .put("/note", ({ note, body: { data } }) => note.add(data), {
+    body: t.Object({
+      data: t.String(),
+    }),
+  })
   .get(
     "/note/:index",
     ({ note, params: { index }, error }) => {
-      return note.data[index] ?? error(404);
+      return note.data[index] ?? error(404, "Not Found :(");
     },
     {
       params: t.Object({
@@ -18,4 +38,32 @@ const app = new Elysia()
       }),
     },
   )
-  .listen(3000);
+  .delete(
+    "/note/:index",
+    ({ note, params: { index }, error }) => {
+      if (index in note.data) return note.remove(index);
+
+      return error(422);
+    },
+    {
+      params: t.Object({
+        index: t.Number(),
+      }),
+    },
+  )
+  .patch(
+    "/note/:index",
+    ({ note, params: { index }, body: { data }, error }) => {
+      if (index in note.data) return note.update(index, data);
+
+      return error(422);
+    },
+    {
+      params: t.Object({
+        index: t.Number(),
+      }),
+      body: t.Object({
+        data: t.String(),
+      }),
+    },
+  );
